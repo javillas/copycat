256dccaeaaa5b544eac2a2d79742a1f68b1edf2b
javillas <aldwinvillas@gmail.com>
2018-07-29 11:58:17 +0800

    revision one

---
+++ Gruntfile.js
@@ -0,0 +1,85 @@
+module.exports = function(grunt) {
+ 
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        /*
+        *SASS TASK
+        */
+        sass:{
+            dev:{
+                options:{
+                    style:'expanded',
+                    sourcemap:'none',
+                },
+                files:{
+                    'process/css/style.css' : 'process/sass/*.scss',
+                }
+            },
+            dist:{
+                options:{
+                    style:'compressed',
+                    sourcemap:'none',
+                },
+                files:{
+                    'process/css/style.min.css' : 'process/sass/*.scss',
+                }
+            }
+        },
+        
+        /*
+        *AUTOPREFIXER TASK
+        */
+        autoprefixer: {
+            options: {
+                browsers:['last 2 versions'],
+            },
+            multiple_files: {
+                expand: true,
+                flatten: true,
+                src:'process/css/*.css',
+                dest:'builds/copycat/css',
+            },
+        },
+        
+        /*
+        *CONNECT TASK
+        */
+        connect: {
+            server: {
+                options: {
+                    hostname: 'localhost',
+                    port: 8000,
+                    base: 'builds/copycat/',
+                    livereload: true
+                }
+            }
+        },
+        
+        /*
+        *WATCH TASK
+        */
+        watch:{
+            options:{
+                livereload: true
+            },
+            css:{
+                files: '**/*.scss',
+                tasks: ['sass','autoprefixer'],
+            },
+            html:{
+                files: 'builds/copycat/*.html',
+                tasks:['connect']
+            }
+        }
+        
+    
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-express-server');
+    
+    grunt.registerTask('default', ['connect','watch']);
+};
---
+++ builds/copycat/js/scripts.js
@@ -0,0 +1,46 @@
+$(function (){
+    
+    "use strict";
+    
+    if($(window).width() < 768){
+        $("#specialCol").addClass("bgGray");
+    }
+    
+    $(window).resize(function(){
+        if($(window).width() < 768){
+            $("#specialCol").addClass("bgGray");
+            $(".wrapper").hide();
+        }else{
+            $("#specialCol").removeClass("bgGray");
+            $(".wrapper").show();
+        }
+    });
+    
+    $(".toggle-button").click(function(){
+        $(".wrapper").slideToggle();
+        
+    });
+    
+    $(".clientSurvey").hover(
+        function(){
+            $(".clientSurvey").css({
+                "background": "url(../img/sidebtn_befhov.png) no-repeat",
+                "background-position": "6px 10px",
+                "background-color": "#1a1478"
+            });
+            $(".clientSurveyText").css({
+                "color":"#fff"
+            });
+        },function(){
+            $(".clientSurvey").css({
+                "background": "url(../img/sidebtn_bef.png) no-repeat",
+                "background-position": "6px 10px",
+                "background-color": ""
+            });
+            $(".clientSurveyText").css({
+                "color":""
+            });
+        }
+    );
+    
+});
